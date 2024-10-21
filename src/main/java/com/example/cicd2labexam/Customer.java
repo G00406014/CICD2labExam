8a44ecbfdbcfe450a954f8899fbbe88b9a9b402c
G00406014@atu.ie <G00406014@atu.ie>
2024-10-21 16:33:21 +0100

    Added H2 datbase and Customer class

--- src/main/java/com/example/cicd2labexam/Customer.java
+++ src/main/java/com/example/cicd2labexam/Customer.java
@@ -1,4 +1,25 @@
 package com.example.cicd2labexam;
 
+
+
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.lang.annotation.Documented;
+
+
+@Data
+@Documented(collection = "customers")
 public class Customer {
+
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+
+
 }
+
+
+
